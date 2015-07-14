cde79ac42358f345b66745ed750e91da1b6108f7
AndroidHell <ralkkai1337@gmail.com>
2015-07-14 19:37:46 +0000

    fixed a few mroe things

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -28,7 +28,7 @@ class PasswordResetsController < ApplicationController
       render 'edit'
     elsif @user.update_attributes(user_params)
       log_in @user
-      flash[:success] = "Password has bee nreset."
+      flash[:success] = "Password has been reset."
       redirect_to @user
     else
       render 'edit'
