6dfbbabfe2a4fd039e3652559d8a0961a1ab1efa
AndroidHell <ralkkai1337@gmail.com>
2015-07-14 02:05:03 +0000

    add account activation

---
+++ app/assets/javascripts/extras.js
@@ -0,0 +1,24 @@
+//this is stuff I added along the way while going through the tutorial
+
+//needed to add the sidebar slide out on smaller screens
+$(document).ready(function() {   
+    var sideslider = $('[data-toggle=collapse-side]');
+    var sel = sideslider.attr('data-target');
+    var sel2 = sideslider.attr('data-target-2');
+    sideslider.click(function(event){
+        $(sel).toggleClass('in');
+        $(sel2).toggleClass('out');
+    });
+});
+
+//needed to get the dropdown working properly.  
+$(document).ready(function() {
+  $(".dropdown-toggle").dropdown();
+});
+
+//animated hamburger button - uses navbar-toggle so no need changing there.
+$(document).ready(function () {
+	  $(".navbar-toggle").on("click", function () {
+		    $(this).toggleClass("active");
+	  });
+});
